3253e410846230cbdae19a566868cd3462efc655
jpmedinagl <jpmedinagl@gmail.com>
2024-11-07 15:26:02 -0500

    - adding common auth function

--- PP2/utils/auth.js
+++ PP2/utils/auth.js
@@ -1,29 +1,53 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
+import prisma from "@/utils/prismaclient";
+
 const BCRYPT_SALT_ROUNDS = parseInt(process.env.BCRYPT_SALT_ROUNDS);
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
 
 export async function hashPassword(password) {
-  return await bcrypt.hash(password, BCRYPT_SALT_ROUNDS);
+	return await bcrypt.hash(password, BCRYPT_SALT_ROUNDS);
 }
 
 export async function comparePassword(password, hash) {
-  return await bcrypt.compare(password, hash);
+	return await bcrypt.compare(password, hash);
 }
 
 export function generateToken(userId, username, role) {
-  return jwt.sign({ userId, username, role }, JWT_SECRET, {
-    expiresIn: JWT_EXPIRES_IN,
-  });
+	return jwt.sign({ userId, username, role }, JWT_SECRET, {
+		expiresIn: JWT_EXPIRES_IN,
+	});
 }
 
-export function verifyToken(token) {
-  try {
-    return jwt.verify(token, JWT_SECRET);
-  } catch (err) {
-    return null;
-  }
+function verifyToken(token) {
+	try {
+		return jwt.verify(token, JWT_SECRET);
+	} catch (err) {
+		return null;
+	}
 }
 
+export async function authUser(authHeader) {
+	if (!authHeader || !authHeader.startsWith("Bearer ")) {
+		return { success: false, status: 401, error: "Unauthorized" };
+	}
+
+	const token = authHeader.split(" ")[1];
+	const decoded = verifyToken(token);
+
+	if (!decoded) {
+		return { success: false, status: 401, error: "Unauthorized Token" };
+	}
+
+	const user = await prisma.user.findUnique({
+		where: { id: decoded.userId }
+	});
+
+	if (!user) {
+		return { success: false, status: 400, error: "User does not exist." };
+	}
+
+	return { success: true, user: user };
+}
