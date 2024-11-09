b1695c47f0eb9d390ba417ec78496968735b47ef
jpmedinagl <jpmedinagl@gmail.com>
2024-11-09 17:08:14 -0500

    adding returned user in template index

--- PP2/pages/api/template/index.js
+++ PP2/pages/api/template/index.js
@@ -56,6 +56,7 @@ export default async function handler(req, res) {
                 tags: true,
                 blogposts: true,
                 forks: true,
+                user: true,
             }
         });
 
