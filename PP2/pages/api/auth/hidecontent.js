a396938fb9321fdd3eb7aeabc9cabb6e7a04c65a
mohammedhussain <moham21387@gmail.com>
2024-11-16 21:37:31 -0500

    comitting hide

--- PP2/pages/api/auth/hidecontent.js
+++ PP2/pages/api/auth/hidecontent.js
@@ -1,56 +1,77 @@
-import prisma from "../../../utils/prismaclient";
-import jwt from 'jsonwebtoken';
-
+import prisma from "@/utils/prismaclient";
 
 export default async function handler(req, res) {
-    if (req.method !== 'POST')
-        return res.status(405).json({message: "need to use POST method"});
-    const {id, isHidden} = req.body; //a bool 
+    if (req.method !== "POST") {
+        return res.status(405).json({ error: "Method not allowed." });
+    }
+
+    const { reportId } = req.body;
   
-    if (!id || typeof isHidden !== 'boolean') { //verify inputs
-      return res.status(400).json({
-        message: "Please provide all the required fields",
-      });
-    }
+    const authHeader = req.headers.authorization;
 
-    const token = req.headers.authorization?.split(' ')[1]; 
-    if (!token) {
-        return res.status(401).json({ message: "Authorization token required" });
-    }
-
-    try
-    {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        if (decoded.role == 'user') {
-            return res.status(403).json({ message: "Access denied: Admins only" });
+    try {
+        // authenticate user
+        const result = await authUser(authHeader);
+        if (result.success === false) {
+            return res.status(result.status).json({ error: result.error });
         }
 
-        const comment = await prisma.comment.findUnique({
-            where: {
-              id, //finding comment based off id 
-            },
-          });
+        const user = result.user;
+        if (user.role !== "admin") {
+            return res.status(400).json({ error: "Admin role required." });
+        }
 
-          if (!comment)
-            return res.status(404).json({message: "no such comment with id exists"});
+        // find report and verify it exists
+        const report = await prisma.report.findUnique({
+            where: { id: reportId }
+        });
 
-        const updatedComment = await prisma.comment.update({ //update isHidden with entered value
-            where: {
-                id,
-            },
-            data: {
-                isHidden : isHidden,
-            },
+        if (!report) {
+            return res.status(400).json({ error: "Report does not exist." });
+        }
 
-        })
-        return res.status(200).json({message: "successful update"})
+        if (report.blogpostId) {
+            // report is of a blogpost: verify and hide the blogpost
+            const blogpost = await prisma.blogpost.findUnique({
+                where: { id: report.blogpostId }
+            });
 
-    }
-    catch(error)
-    {
-        console.error("error updating", error)
-        return res.status(500).json({message: "error"})
+            if (!blogpost) {
+                return res.status(400).json({ error: "Report not associated with existing blogpost." });
+            }
+
+            const hiddenBlogpost = await prisma.blogpost.update({
+                where: { id: report.blogpostId },
+                data: {
+                    isHidden: true,
+                }
+            });
+
+            return res.status(200).json(hiddenBlogpost);
+        } else if (report.commentId) {
+            // report is of a comment: verify and hide the comment
+            const comment = await prisma.comment.findUnique({
+                where: { id: report.commentId }
+            });
+
+            if (!comment) {
+                return res.status(400).json({ error: "Report not associated with existing comment." });
+            }
+
+            const hiddenComment = await prisma.comment.update({
+                where: { id: report.commentId },
+                data: {
+                    isHidden: true,
+                }
+            });
+
+            return res.status(200).json(hiddenComment);
+        } else {
+            // shouldn't happen, if a blogpost or comment is deleted, report should also be deleted
+            return res.status(500).json({ error: "Report not associated with existing blogpost or comment." });
+        }
+    } catch (error) {
+        return res.status(500).json({ error: "Error blocking content." });
     }
 }
 
