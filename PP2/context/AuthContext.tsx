7ce664bd7f4ec502e4e430ac58cda0ff0920b57c
jpmedinagl <jpmedinagl@gmail.com>
2024-11-22 11:18:09 -0500

    adding authentication role

--- PP2/context/AuthContext.tsx
+++ PP2/context/AuthContext.tsx
@@ -6,7 +6,8 @@ type AuthContextType = {
 	username: string | null; // Add username
 	avatar: string | null;
 	token: string | null;
-	login: (token: string, userId: string, username: string, avatar: string) => void; // Include username in login
+	role: string | null;
+	login: (token: string, userId: string, username: string, avatar: string, role: string) => void; // Include username in login
 	logout: () => void;
 	update: (username: string, avatar: string) => void;
 };
@@ -19,6 +20,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 	const [username, setUsername] = useState<string | null>(null); // State for username
 	const [avatar, setAvatar] = useState<string | null>(null);
 	const [token, setToken] = useState<string | null>(null);
+	const [role, setRole] = useState<string | null>(null);
 
 	useEffect(() => {
 		// Check for token, userId, and username in localStorage
@@ -26,13 +28,15 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 		const savedUserId = localStorage.getItem("userId");
 		const savedUsername = localStorage.getItem("username");
 		const savedAvatar = localStorage.getItem("avatar");
+		const savedRole = localStorage.getItem("role");
 
-		if (token && savedUserId && savedUsername && savedAvatar) {
+		if (token && savedUserId && savedUsername && savedAvatar && savedRole) {
 			setIsAuthenticated(true);
 			setUserId(savedUserId);
 			setUsername(savedUsername);
 			setAvatar(savedAvatar); // Set avatar
 			setToken(token);
+			setRole(savedRole);
 		}
 	}, []);
 
@@ -43,35 +47,39 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 		setAvatar(avatar);
 	}
 
-	const login = (token: string, userId: string, username: string, avatar: string) => {
+	const login = (token: string, userId: string, username: string, avatar: string, role: string) => {
 		localStorage.setItem("userToken", token);
 		localStorage.setItem("userId", userId);
 		localStorage.setItem("username", username); // Store username in localStorage
 		localStorage.setItem("avatar", avatar); // Save avatar URL
+		localStorage.setItem("role", role);
 		setIsAuthenticated(true);
 		setUserId(userId);
 		setUsername(username);
 		setAvatar(avatar);
 		setToken(token);
+		setRole(role);
 	};
 
 	const logout = () => {
 		if (typeof window !== "undefined") {
 		// Only access localStorage on the client-side
-		localStorage.removeItem("userToken");
-		localStorage.removeItem("userId");
-		localStorage.removeItem("username"); // Remove username from localStorage
-		localStorage.removeItem("avatar");
+			localStorage.removeItem("userToken");
+			localStorage.removeItem("userId");
+			localStorage.removeItem("username"); // Remove username from localStorage
+			localStorage.removeItem("avatar");
+			localStorage.removeItem("role");
 		}
 		setIsAuthenticated(false);
 		setUserId(null);
 		setUsername(null);
 		setAvatar(null);
 		setToken(null);
+		setRole(null);
 	};
 
 	return (
-		<AuthContext.Provider value={{ isAuthenticated, userId, username, avatar, token, login, logout, update }}>
+		<AuthContext.Provider value={{ isAuthenticated, userId, username, avatar, token, role, login, logout, update }}>
 			{children}
 		</AuthContext.Provider>
 	);
