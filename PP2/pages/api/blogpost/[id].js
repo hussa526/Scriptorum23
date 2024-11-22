7aed24ff670c07b864b89c0eda6f2f070d5a5613
jpmedinagl <jpmedinagl@gmail.com>
2024-11-22 11:16:29 -0500

    fixing backend

--- PP2/pages/api/blogpost/[id].js
+++ PP2/pages/api/blogpost/[id].js
@@ -10,25 +10,26 @@ export default async function handler(req, res) {
 
     try {
         const blogpost = await prisma.blogpost.findUnique({
-            where: { id: blogpostId },
-            include: {
-              templates: {
-                include: {
-                  user: true, // Ensure this is properly structured and that the `Template` model has a `user` relation
-                  tags: true,
-                },
-              },
-              user: true,
-              tags: true,
-              comments: {
-                include: {
-                  user: true,
-                  votes: true,
-                },
-              },
-              votes: true,
-            },
-          });
+			where: { id: blogpostId },
+			include: {
+				templates: {
+				include: {
+					user: true, // Ensure this is properly structured and that the `Template` model has a `user` relation
+					tags: true,
+				},
+				},
+				user: true,
+				tags: true,
+				comments: {
+				include: {
+					user: true,
+					votes: true,
+				},
+				},
+				reports: true,
+				votes: true,
+			},
+		});
           
           
 
